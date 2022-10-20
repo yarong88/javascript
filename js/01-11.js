64bb991b39ffb36d9dbb9aaa713eff6633a34d7e
yarong88 <yarong1818@gmail.com>
2022-10-20 17:41:36 +0900

    10월20일

---
+++ js/01-11.js
@@ -0,0 +1,31 @@
+let number = prompt("값을 입력하세요.");
+let table = "";
+table += `<table>`
+table += `   <tr>`
+table += `       <th>증감연산자</th>`
+table += `       <th>설명</th>`
+table += `      <th>예제 let number = ${number}</th>`
+table += ` </tr>`
+table += `   <tr>`
+table += `       <td>변수++</td>`
+table += `       <td>기존의 변수 값에 1을 더합니다. (후위)</td>`
+table += `       <td>alert(number++) = ${number++}</td>`
+table += `   </tr>`
+table += `   <tr>`
+table += `       <td>++변수</td>`
+table += `       <td>기존의 변수 값에 1을 더합니다. (전위)</td>`
+table += `       <td>alert(++number) = ${++number}</td>`
+table += `   </tr>`
+table += `   <tr>`
+table += `       <td>변수--</td>`
+table += `       <td>기존의 변수 값에 1을 뺍니다. (후위)</td>`
+table += `       <td>alert(number--) = ${number--}</td>`
+table += `   </tr>`
+table += `   <tr>`
+table += `       <td>변수++</td>`
+table += `       <td>기존의 변수 값에 1을 뺍니다. (전위)</td>`
+table += `       <td>alert(--number) = ${--number}</td>`
+table += `   </tr>`
+table += `</table>`
+
+document.write(table);
