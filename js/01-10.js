0451a64d1cdfdc37515dba999769360d793cfeed
yarong88 <yarong1818@gmail.com>
2022-10-20 16:29:10 +0900

    10월20일

---
+++ js/01-10.js
@@ -0,0 +1,73 @@
+
+        let list = '';
+
+        list += '<ul>';
+        list += '   <li>Hello</li>';
+        list += '   <li>JavaScript..!</li>';
+        list += '<ul>';
+
+        document.write(list);
+
+        let table = '';
+
+        table += '<table>' 
+        table += '  <tr>'
+        table += '      <th>&&</th>' 
+        table += '      <th>and</th>' 
+        table += '  </tr>'
+        table += '  <tr>'
+        table += '      <td>false</td>' 
+        table += '      <td>false</td>' 
+        table += '      <td>false</td>' 
+        table += '  </tr>'
+        table += '  <tr>'
+        table += '      <td>true</td>' 
+        table += '      <td>false</td>' 
+        table += '      <td>false</td>' 
+        table += '  </tr>'
+        table += '  <tr>'
+        table += '      <td>false</td>' 
+        table += '      <td>true</td>' 
+        table += '      <td>false</td>' 
+        table += '  </tr>'
+        table += '  <tr>'
+        table += '      <td>true</td>' 
+        table += '      <td>true</td>' 
+        table += '      <td>true</td>' 
+        table += '  </tr>'
+
+        table += '</table>' 
+        
+        document.write(table);
+
+        let table02 = '';
+
+        table02 += '<table>' 
+        table02 += '  <tr>'
+        table02 += '      <th>||</th>' 
+        table02 += '      <th>or</th>' 
+        table02 += '  </tr>'
+        table02 += '  <tr>'
+        table02 += '      <td>false</td>' 
+        table02 += '      <td>false</td>' 
+        table02 += '      <td>false</td>' 
+        table02 += '  </tr>'
+        table02 += '  <tr>'
+        table02 += '      <td>true</td>' 
+        table02 += '      <td>false</td>' 
+        table02 += '      <td>true</td>' 
+        table02 += '  </tr>'
+        table02 += '  <tr>'
+        table02 += '      <td>false</td>' 
+        table02 += '      <td>true</td>' 
+        table02 += '      <td>true</td>' 
+        table02 += '  </tr>'
+        table02 += '  <tr>'
+        table02 += '      <td>true</td>' 
+        table02 += '      <td>true</td>' 
+        table02 += '      <td>true</td>' 
+        table02 += '  </tr>'
+
+        table02 += '</table>' 
+        
+        document.write(table02);
